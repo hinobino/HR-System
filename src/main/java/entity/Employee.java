5e8b4e5b5c3aaa3147f3bf4f833db66f2b5dd95e
Jake Cohen <jakecohen@Jakes-MacBook-Air-4.local>
2024-11-07 19:35:46 -0500

    Updated User abstract class and wrote Employee subclass.

--- src/main/java/entity/Employee.java
+++ src/main/java/entity/Employee.java
@@ -1,4 +1,34 @@
 package entity;
 
-public class Employee {
+/**
+ * The representation of an employee.
+ */
+public class Employee extends User {
+
+    private String status = "INACTIVE";
+
+    /**
+     * Constructor for an Employee object.
+     * @param userid the id of the employee.
+     * @param password the password of the employee.
+     */
+    public Employee(String userid, String password) {
+        super(userid, password);
+    }
+
+    /**
+     * Return the current status of the Employee.
+     * @return the status of the Employee.
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Change the status of the employee to ACTIVE or INACTIVE.
+     * @param status the new status of the employee.
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,19 +1,35 @@
 package entity;
 
+/**
+ * The representation of a user.
+ */
 public abstract class User {
-    String userid;
-    String password;
+    private final String userid;
+    private final String password;
 
-    public User(String password, String userid) {
-        this.password = password;
+    /**
+     * Constructor for a basic user object.
+     * @param userid the ID of the user.
+     * @param password the password of the user.
+     */
+    protected User(String userid, String password) {
         this.userid = userid;
+        this.password = password;
     }
 
+    /**
+     * Returns the user's ID.
+     * @return the ID of the user.
+     */
     public String getUserid() {
         return userid;
     }
 
-    public void setUserid(String userid) {
-        this.userid = userid;
+    /**
+     * Returns the user's password.
+     * @return the password of the user.
+     */
+    public String getPassword() {
+        return password;
     }
 }
