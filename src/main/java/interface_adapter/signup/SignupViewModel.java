039abe5a1688b553b227edece89b3317e785b9e5
Jake Cohen <jakecohen@Jakes-MacBook-Air-4.local>
2024-11-10 00:12:51 -0500

    Completed signup files in interface_adapter. Renamed login files in interface_adapter to be consistent, i.e. Login not LogIn.

--- src/main/java/interface_adapter/signup/SignupViewModel.java
+++ src/main/java/interface_adapter/signup/SignupViewModel.java
@@ -1,4 +1,25 @@
-package main.java.interface_adapter.signup;
+package interface_adapter.signup;
 
-public class SignupViewModel {
+import interface_adapter.ViewModel;
+
+/**
+ * View Model for the Signup View.
+ */
+public class SignupViewModel extends ViewModel<SignupState> {
+
+    public static final String TITLE_LABEL = "Signup View";
+    public static final String USERID_LABEL = "Enter User ID";
+    public static final String PASSWORD_LABEL = "Choose Password";
+
+    public static final String SIGNUP_BUTTON_LABEL = "Sign Up";
+    public static final String TO_WELCOME_BUTTON_LABEL = "Back to Welcome Page";
+
+    public SignupViewModel() {
+        // this creates a view model with "sign up" as the title
+        super("sign up");
+
+        // this makes sure the state uses the SignupState we created
+        setState(new SignupState());
+    }
+
 }
