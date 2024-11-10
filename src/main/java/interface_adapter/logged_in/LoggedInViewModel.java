90fc1993cf333faffea1eeb16c77c2fcbe3a616c
Emmett <180994452+eruss341@users.noreply.github.com>
2024-11-09 23:24:33 -0500

    Wrote Logged_in interface adapter files

--- src/main/java/interface_adapter/logged_in/LoggedInViewModel.java
+++ src/main/java/interface_adapter/logged_in/LoggedInViewModel.java
@@ -1,4 +1,15 @@
 package main.java.interface_adapter.logged_in;
 
-public class LoggedInViewModel {
+import interface_adapter.ViewModel;
+
+/**
+ * The View Model for the Logged In View.
+ */
+public class LoggedInViewModel extends ViewModel<LoggedInState> {
+
+    public LoggedInViewModel() {
+        super("logged in");
+        setState(new LoggedInState());
+    }
+
 }
