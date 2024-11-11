b989fe170113c1420abee92423e78f2dd1add7b9
Jake Cohen <jakecohen@Jakes-MacBook-Air-4.local>
2024-11-11 15:03:11 -0500

    Added label constants to WelcomeViewModel and role instance variable to WelcomeState.

--- src/main/java/interface_adapter/welcome/WelcomeState.java
+++ src/main/java/interface_adapter/welcome/WelcomeState.java
@@ -4,4 +4,15 @@ package interface_adapter.welcome;
  * The state class for the WelcomeViewModel
  */
 public class WelcomeState {
+
+    public String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public String setRole(String role) {
+        this.role = role;
+    }
+
 }
