3cda204c386b6c79ac8c7a099c427e7cfee77df3
Jake Cohen <jakecohen@Jakes-MacBook-Air-4.local>
2024-11-17 22:51:00 -0500

    When an inactive employee is created, it is now added to the respective Manager's list of employee objects.

--- src/main/java/use_case/create_employee/CreateEmployeeInteractor.java
+++ src/main/java/use_case/create_employee/CreateEmployeeInteractor.java
@@ -2,6 +2,7 @@ package use_case.create_employee;
 
 import entity.Employee;
 import entity.EmployeeFactory;
+import entity.Manager;
 
 /**
  * The Create Employee Interactor.
@@ -36,6 +37,10 @@ public class CreateEmployeeInteractor implements CreateEmployeeInputBoundary {
             // Creates a new Employee with the given user ID.
             Employee inactiveEmployee = employeeFactory.create(createEmployeeInputData.getNewUserID(), "");
             userDataAccessObject.save(inactiveEmployee);
+
+            // Add the new employee to the list of the manager's employees
+            Manager currentManager = (Manager) userDataAccessObject.get(userDataAccessObject.getCurrentUserID());
+            currentManager.addEmployee(inactiveEmployee);
 
             final CreateEmployeeOutputData createEmployeeOutputData = new CreateEmployeeOutputData(
                     createEmployeeInputData.getNewUserID(),
