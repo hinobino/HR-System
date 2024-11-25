aeb01f3febbb07301e708aeee7908150b7a6e88b
Jake Cohen <jakecohen@Jakes-MacBook-Air-4.local>
2024-11-25 13:48:06 -0500

    Updated start and end time inputs in schedule shift to only accept 30-minute intervals from 9am to 5pm via a selector.

--- src/main/java/view/TimeFormatter.java
+++ src/main/java/view/TimeFormatter.java
@@ -6,17 +6,17 @@ import java.time.LocalTime;
 
 public class TimeFormatter {
 
-    MaskFormatter timeFormat;
-
-    public TimeFormatter() {
-        try {
-            this.timeFormat = new MaskFormatter("##:##");
-            this.timeFormat.setPlaceholderCharacter('_');
-            this.timeFormat.setAllowsInvalid(false);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
+    static final String[] VALID_TIMES = {
+            "09:00", "09:30",
+            "10:00", "10:30",
+            "11:00", "11:30",
+            "12:00", "12:30",
+            "13:00", "13:30",
+            "14:00", "14:30",
+            "15:00", "15:30",
+            "16:00", "16:30",
+            "17:00"
+    };
 
     /**
      * Check if a given time string is valid.
