ef97df8072fc1b0144c3b3fa6642d5e48039d80f
hinobino <hinar.alsharkawy@mail.utoronto.ca>
2024-12-01 21:10:21 -0500

    'Updated EC use case files.'

--- src/main/java/use_case/export_calendar/ExportCalendarOutputBoundary.java
+++ src/main/java/use_case/export_calendar/ExportCalendarOutputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.export_calendar;
 
 public interface ExportCalendarOutputBoundary {
+    /**
+     * Prepares the success view for the ExportCalendar use case.
+     * @param outputData the output data.
+     */
+    void prepareSuccessView(ExportCalendarOutputData outputData);
 }
