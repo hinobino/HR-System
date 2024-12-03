9800507636e5e7b29d2ad3147cc0fe2b38a50712
hinobino <hinar.alsharkawy@mail.utoronto.ca>
2024-12-03 10:17:07 -0500

    'Export calendar presenter and controller completed.'

--- src/main/java/interface_adapter/export_calendar/ExportCalendarController.java
+++ src/main/java/interface_adapter/export_calendar/ExportCalendarController.java
@@ -1,4 +1,26 @@
 package interface_adapter.export_calendar;
 
+import entity.Employee;
+import use_case.export_calendar.ExportCalendarInputBoundary;
+import use_case.export_calendar.ExportCalendarInputData;
+import use_case.login.LoginInputData;
+import view.LoginView;
+
 public class ExportCalendarController {
+    private final ExportCalendarInputBoundary exportCalendarInteractor;
+
+    public ExportCalendarController(ExportCalendarInputBoundary exportCalendarInteractor) {
+        this.exportCalendarInteractor = exportCalendarInteractor;
+    }
+
+    /**
+     * Executes the ExportCalendar Use Case.
+     *
+     * @param employee the employee
+     */
+    public void execute(Employee employee) {
+        final ExportCalendarInputData exportCalendarInputData = new ExportCalendarInputData(employee);
+
+        exportCalendarInteractor.execute(exportCalendarInputData);
+    }
 }
